227775d29e3dce53b4e44cdb5f4574b60c74ad8b
tahmingithub <tahminnka@gmail.com>
2023-11-01 19:24:25 -0400

    this commit has the basic structor of solar system

---
+++ src/test/java/SolarSystem/main.java
@@ -0,0 +1,13 @@
+package SolarSystem;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class main {
+    public static void main (String[] args){
+        solarSystemBasics solarsys=new solarSystemBasics();
+        solarsys.Sun="1";
+        solarsys.Moon="2";
+        solarsys.Planets="2";
+        solarsys.Stars="10";
+    }
+}
---
+++ src/test/java/SolarSystem/solarSystemBasics.java
@@ -0,0 +1,10 @@
+package SolarSystem;
+
+public class solarSystemBasics {
+
+    String Sun;
+    String Moon;
+    String Stars;
+    String Planets;
+
+}
