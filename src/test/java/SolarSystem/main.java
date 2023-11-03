2e0b21672104f63fb185c04f7ee62443b0bc5477
tahmingithub <tahminnka@gmail.com>
2023-11-03 14:07:20 -0400

    this commit has feature sun added

--- src/test/java/SolarSystem/main.java
+++ src/test/java/SolarSystem/main.java
@@ -9,5 +9,12 @@ public class main {
         solarsys.Moon="2";
         solarsys.Planets="2";
         solarsys.Stars="10";
+
+       // *****FeatureSun****
+
+        FeatureSun sun= new FeatureSun();
+        sun.name="panther";
+        sun.color="black";
+        sun.temp="1234555";
     }
 }
